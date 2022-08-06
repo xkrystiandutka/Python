83c8c251dae6133db5e4c64f8e2485fa724b52cc
Krystian <Krystian.Dutkait@gmail.com>
2022-08-06 11:31:08 +0200

    Create folder & 01_Print.py file

---
+++ Python/Basics/01_Print.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 11:17:01 2022
+
+@author: Krystian
+"""
+
+# print(2+2)
+
+print(2 + 2)
+
+print(3 * 3)
+
+3 + 2 * 2
+4 - 2 * 2
+
+print(3 + 2 * 2)
+
+10 / 3
+4 / 2
+
+10 // 3 
+
+10 % 3
+
+12 % 5 
+
+13 % 2
+
+(5 - 2 **3 ) * 10
+
+x = 10
+y = 20
+
+z = x * y
+
+print(z)
+
+print('love python')
+print("love python")
+
+"It's the best!"
+
+print('It\'s the best!')
+
+print('Python\n3.9')
+
+print("""
+      Python
+      3.9
+      """)
+
+
