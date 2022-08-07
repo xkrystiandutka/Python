c2a9e7b8f57fccb86b28494847deb53d5ae5fa9c
Krystian <Krystian.Dutkait@gmail.com>
2022-08-07 16:03:43 +0200

    Data types

---
+++ Python/Basics/05_DataTypes.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 15:57:03 2022
+
+@author: Krystian
+"""
+
+string = 'Python'
+
+print(dir(string))
+
+# %%
+
+a = 10
+
+print(dir(a))
+
+# %%
+
+type(a)
+
+# %%
+
+b = 4.5
+type(b)
+
+# %% 
+
+d = 3 + 3j
+type(d)
+
+# %%
+
+type(True)
+type(False)
+
+value = True
