c2db9bf0855b173e13db3ba43f7749bb718b188e
Krystian <Krystian.Dutkait@gmail.com>
2022-08-07 15:35:36 +0200

    Add input file

---
+++ Python/Basics/02_Input.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  7 15:30:04 2022
+
+@author: Krystian
+"""
+
+# %%
+imie = input('Podaj swoje imie: ')
+
+# print('Czesc', imie)
+print('Czesc {}!'.format(imie))
+
+# %%
+jezyk = input('Jakiego języka chcesz się nauczyć? ')
+
+print('Podano język {}'.format(jezyk))
+
+# %%
+name = input('Podaj imie: ')
+age = input('Podaj wiek: ')
+
+print('Czesc {}! Masz {} lat.'.format(name, age))
+
+# %%
+age = int(input('Podaj wiek: '))
