2c342f8a022de3cffc71b42842124ec5c8d08663
Krystian <Krystian.Dutkait@gmail.com>
2022-08-07 15:45:17 +0200

    Variable naming convention

---
+++ Python/Basics/03_Variables.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 15:37:00 2022
+
+@author: Krystian
+"""
+
+name = 'Krystian'
+
+_name = 'Krystian'
+
+#3name = 'It doesn't work'
+
+# %%
+
+a = 8
+b = 20
+c = a * b
+
+print(c)
+
+# %%
+
+partyTime = 4
+salary = 30
+
+salaryPerDay = partyTime * salary
+print(salaryPerDay)
+
+# %%
+
+camelCase = 'Python 3.9'
+PascalCase = 'Python 3.9'
+snake_case = 'Python 3.9'
+kebab_case = 'Python 3.9'
+UPPER = 'Python 3.9'
+
+# %%
+
+import keyword
+keyword.kwlist
