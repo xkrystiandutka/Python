9e2b884ee0291492a1b5c45674750370bbb7849a
Krystian <Krystian.Dutkait@gmail.com>
2022-08-08 09:47:58 +0200

    Lists

---
+++ Python/DataStructures/03_Lists.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 09:38:26 2022
+
+@author: Krystian
+"""
+empty_list = list()
+print(empty_list)
+
+# %%
+techs = ['python', 'java', 'c++', 'go', 'sql']
+techs[0] = 'python 3.7'
+print(techs)
+
+# %%
+numbers = [3, 5, 3, 5, 23]
+print(numbers)
+print(type(numbers))
+
+# %%
+mixed = ['python', 3.7, 4, True]
+print(mixed)
+
+# %%
+empty = []
+nested = [[1, 2, [3, 'sql']], ['python', 'java', 'go'], 3]
+
+# %%
+first = ['mleko', 'ziemniaki', 'makaron']
+second = ['woda', 'jajka']
+
+bucket = [first, second]
+
+# %%
+len(bucket)
+
+# %%
+techs = ['python', 'java', 'c++', 'go', 'sql']
+
+techs += ['javascript']
+print(techs)
+
+# %%
+print(dir(list))
