823030be5ae490028735bf1fd5072b0ca9cda186
Krystian <Krystian.Dutkait@gmail.com>
2022-08-08 10:02:01 +0200

    Lists cutting

---
+++ Python/DataStructures/04_ListsCutting.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 09:52:18 2022
+
+@author: Krystian
+"""
+
+index = [0, 1, 2, 3, 4, 5]
+list1 = [34, 23, 56, 24, 23, 76]
+idx_n = [-6, -5, -4, -3, -2, -1]
+
+# list1[start:stop]
+# list1[index]
+# list1[start:]
+# list1[:stop]
+# list1[::step]
+
+list1[0]
+list1[1]
+list1[-1]
+list1[-3]
+
+# %%
+list1[1:5]
+list1[-5:-1]
+list1[::-1]
+
+# %%
+list1[:4]
+
+list1[3:]
