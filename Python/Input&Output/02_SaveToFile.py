07afe91c2ad616b23221f151172597f76d36027b
Krystian <Krystian.Dutkait@gmail.com>
2022-08-10 11:02:03 +0200

    Save to file example

--- Python/Input&Output/02_SaveToFile.py
+++ Python/Input&Output/02_SaveToFile.py
@@ -40,3 +40,14 @@ with open('techs.txt', 'r') as file:
     for line in file:
         technologies.append(line[:-1])
 print(technologies)
+
+
+# %%
+with open('tree.txt', 'w') as file:
+    for j in range(2):
+        for i in range(10):
+            print('{:>9}'.format('*' * i), end='', file=file)
+            print('{}'.format('*' * i), file=file)
+
+
+
