cc8a7ea56466eff39e9fb16fe3434d7ed033e243
Krystian <Krystian.Dutkait@gmail.com>
2022-08-10 18:25:06 +0200

    Function documentation

---
+++ Python/Functions/03_docs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 18:19:50 2022
+
+@author: Krystian
+"""
+
+def add(a, b):
+    """Zwraca sumę dwóch liczb.
+    Inputs:
+        a: int
+        b: int
+    Outputs:
+        sum: int"""
+    return a + b
+
+# %%
+help(add)
