a362672a0248095db6428d2c9541752797909287
Krystian <Krystian.Dutkait@gmail.com>
2022-08-11 21:26:19 +0200

    Modules

---
+++ Python/Modules&Packages/analysis.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 21:21:09 2022
+
+@author: Krystian
+"""
+
+import rocketScience
+
+dir(rocketScience)
+
+# %%
+
+rocketScience.calculateMean([3, 4])
+rocketScience.calculateMin([3, 4])
+rocketScience.calculateMax([3, 4])
+
+
+# %%
+
+from rocketScience import calculateMean
+
+# %%
+calculateMean([2, 3])
+
+# %%
+from rocketScience import *
+
+# %%
+calculateMean([4, 2])
+
+# %%
+from rocketScience import calculateMin, calculateMax
+
+# %%
+calculateMin([4, 5])
+calculateMax([4, 5])
---
+++ Python/Modules&Packages/rocketScience.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 21:18:33 2022
+
+@author: Krystian
+"""
+
+def calculateMean(x):
+    return sum(x) / len(x)
+
+def calculateMin(x):
+    return min(x)
+
+def calculateMax(x):
+    return max(x)
