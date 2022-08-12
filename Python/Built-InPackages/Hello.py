1265940cce70c389b16d96e3e0fd0378211bc653
Krystian <Krystian.Dutkait@gmail.com>
2022-08-12 11:56:59 +0200

    Packages Sys running scripts from PowerShell & Bash

---
+++ Python/Built-InPackages/Hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 11:52:04 2022
+
+@author: Krystian
+"""
+
+print('Hello world!')
+
+for i in range(10):
+    print('*' * i)
---
+++ Python/Built-InPackages/PackagesSYS.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 11:50:28 2022
+
+@author: Krystian
+"""
+
