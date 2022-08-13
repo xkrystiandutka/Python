23250c8a34c8af161c44e1474af4ddcf1d3ff02d
Krystian <Krystian.Dutkait@gmail.com>
2022-08-13 06:24:19 +0200

    Update warehouse project
    User panel added & if condition fixed in deleteProduct function.

--- Python/OOP/05_Warehouse.py
+++ Python/OOP/05_Warehouse.py
@@ -5,6 +5,8 @@ Created on Fri Aug 12 19:19:18 2022
 @author: Krystian
 """
 
+import sys
+
 class Warehouse:
     
     def __init__(self, listProducts):
@@ -25,9 +27,28 @@ class Warehouse:
         
     def deleteProduct(self):
         self.nameProduct = input('Enter the names of the products you want to remove from the list: >>> ')
-        if self.nameProduct in self.nameProduct:
+        if self.nameProduct in self.listProducts:
             self.listProducts.remove(self.nameProduct)
             print('Product removed from stock.')
         else:
             print('The specified product is not in stock.')
+            
+warehouse = Warehouse(['milk', 'water', 'eggs'])
+
+while True:
+    print('\n')
+    print('Enter 1 to display the stock status.')
+    print('Enter 2 to add a product.')
+    print('Enter 3 to remove the product.')
+    print('Enter 4 to finish.')
+    userSelect = int(input('>>> '))
+    if userSelect == 1:
+        warehouse.displayAvailableProducts()
+    elif userSelect == 2:
+        warehouse.addProduct()
+    elif userSelect == 3:
+        warehouse.deleteProduct()
+    elif userSelect == 4:
+        sys.exit()
+
     
